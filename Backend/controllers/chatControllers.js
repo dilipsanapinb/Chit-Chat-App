ba181db1ae592fe80a3ddf03a6e9d6a7c04e4063
dilipsanapinb <dilipinbsanap@gmail.com>
2023-04-06 17:39:41 +0530

    create chats route added

--- Backend/controllers/chatControllers.js
+++ Backend/controllers/chatControllers.js
@@ -23,3 +23,41 @@ const getChat = asynchandler(async (req, res) => {
         throw new Error(error.message);
     }
 });
+
+// create a new Chat
+const createChat=asynchandler(async(req,res) => {
+    const { userId } = req.body;
+    if (!userId) {
+        console.log("UserId param not sent with request");
+        return res.sendStatus(400)
+    }
+    let isChat = await Chat.find({
+        isGroupChat: false,
+        $and: [
+            { users: { $elemMatch: { $eq: req.user._id } } },
+            { users: { $elemMatch: { $eq: userId } } },
+        ]
+    }).populate("users", "-password")
+        .populate("latestMessage");
+    isChat = await User.populate(isChat, {
+        path: 'latestMessage.sender',
+        select: "name pic email"
+    })
+    if (isChat.length>0) {
+        res.send(isChat[0])
+    } else {
+        var chatData = {
+            chatName: "sender",
+            isGroupChat: false,
+            users: [req.user.id, userId]
+        };
+        try {
+            const createdChat = await Chat.create(chatData);
+            const FullChat = await Chat.findOne({ _id: createdChat._id }).populate("users", "-password");
+            res.status(200).json(FullChat)
+        } catch (error) {
+            res.status(400);
+            throw new Error(error.message);
+        }
+    }
+})
