4613add215cfb79dcc3da294194f41d023be8ed3
dilipsanapinb <dilipinbsanap@gmail.com>
2023-04-06 17:41:45 +0530

    user can bechats route added

--- Backend/controllers/chatControllers.js
+++ Backend/controllers/chatControllers.js
@@ -109,3 +109,25 @@ const renameGroup = asynchandler(async(req,res) => {
         res.json(updateGroupName)
     }
 })
+
+// remove from group
+const removeGroup = asynchandler(async(req,res) => {
+    const { chatId, userId } = req.body;
+
+    const removed = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+            $pull: { users: userId },
+        }, {
+        new: true
+    },
+    )
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password");
+    if (!removed) {
+        res.status(404);
+        throw new Error("Chat Not Found");
+    } else {
+        res.json(removed)
+    }
+})
