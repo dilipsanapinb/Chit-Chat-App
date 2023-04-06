f57d4f9eb214b8627c110d44ae753f7e7ef6ce5f
dilipsanapinb <dilipinbsanap@gmail.com>
2023-04-06 17:40:17 +0530

    create group  chats route added

--- Backend/controllers/chatControllers.js
+++ Backend/controllers/chatControllers.js
@@ -61,3 +61,32 @@ const createChat=asynchandler(async(req,res) => {
         }
     }
 })
+
+// create a group chat
+const groupChat = asynchandler(async(req,res) => {
+    if (!req.body.users || !req.body.name) {
+        return res.status(400).send({ message: "Please Fill All the Fields" });
+    }
+    var users = JSON.parse(req.body.users);
+    if (users.length < 2) {
+        return res.status(400)
+            .send("More thrn two users are required to form a group chat");
+    }
+    users.push(req.user)
+    try {
+        const groupChat = await Chat.create({
+            chatName:req.body.name,
+            users: users,
+            isGroupChat: true,
+            groupAdmin:req.user
+        })
+        const FullGroupChat = await Chat.findOne({ _id: groupChat._id })
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password");
+        res.status(200).json(FullGroupChat);
+
+    } catch (error) {
+        res.status(400);
+        throw new Error(error.message)
+    }
+})
